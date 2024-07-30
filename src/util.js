36cdc678f7a2e522411bdffac85b026acae8ad37
Lea Verou <lea@verou.me>
2024-07-30 18:51:18 +0300

    Remove entities before slugifying

--- src/util.js
+++ src/util.js
@@ -1,6 +1,7 @@
 export function slugify (text) {
 	return (text + "")
 			.normalize("NFD").replace(/[\u0300-\u036f]/g, "") // Convert accented letters to ASCII
+			.replace(/&\w+;/g, "") // Remove HTML entities
 			.replace(/[^\w\s-]/g, "") // Remove remaining non-ASCII characters
 			.trim().replace(/\s+/g, "-") // Convert whitespace to hyphens
 			.toLowerCase();
